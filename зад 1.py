fbc78888f6f9950664ed081d47934137ab333d07
Koregauo <artemzilinskij02@gmail.com>
2023-10-11 09:23:28 +0300

    не туда

--- зад 1.py
+++
@@ -1,7 +0,0 @@
-a=('алуфа 8', 'кальево 684', 'дорофьево 3', 'августина 15', 'рало 59', 'советская 1', 'ярово 87')
-new_tu=a[0:3]
-print(new_tu)
-new_tu2=a[3:]
-print(new_tu2)
-all_tu=(new_tu+new_tu2)
-print(all_tu)
